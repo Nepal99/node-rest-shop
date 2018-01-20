62bf821c808a59b58b082c7a5478ec7b65ef7582
Nepal99 <reddy.nepal571@gmail.com>
2018-01-20 02:03:45 -0500

    04. v6 MongoDB and Mongoose
    Created a MongoDB cluster.
    =>connect to application
    =>authenttication
    =>perform database level REST API operations

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express =  require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://node-rest-shop:'+ 
+process.env.MONGO_ATLAS_PWD
+ +'@node-rest-shop-shard-00-00-b1rxr.mongodb.net:27017,node-rest-shop-shard-00-01-b1rxr.mongodb.net:27017,node-rest-shop-shard-00-02-b1rxr.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin')
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended : false}));
