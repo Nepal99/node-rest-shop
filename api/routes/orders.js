47683a04f977a024b07fbb4b2b5e7d601a2a84b7
Nepal99 <reddy.nepal571@gmail.com>
2018-02-08 18:11:23 -0500

    v9 - Populating Queries with mongoose
    -> Using populate() we can display the models data from the other schemas

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ router.get('/', (req,res,next) => {
    Order
    .find()
    .select('product quantity _id')
+   .populate('product', ['name', 'price'])
    .exec()
    .then(docs => {
        console.log(docs);
@@ -107,6 +108,7 @@ router.post('/', (req,res,next) => {
 
 router.get('/:orderId', (req,res,next) => {
     Order.findById(req.params.orderId)
+    .populate('product')
     .exec()
     .then(order => {
         if(!order){
